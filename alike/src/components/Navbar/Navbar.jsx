26e30dd56b5f105d54a1dba8d1791828a70a0b55
hairyElephant <josecalderon353@gmail.com>
2023-02-09 13:46:18 -0500

    Added Navbar component folder

--- alike/src/components/Navbar/Navbar.jsx
+++ alike/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React from 'react'
+import './navbar.css'
 
-export default function Navbar() {
-  return <div>Navbar</div>;
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
 }
+
+export default Navbar
