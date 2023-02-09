4fdb6da2889979366c3f9c33200f44c9daeaaaeb
anna <anna@rcbd.org>
2023-02-09 15:01:24 -0500

    Add package.json

--- alike/src/App.js
+++ alike/src/App.js
@@ -1,21 +1,18 @@
+//import React
+import { Route, Routes } from "react-router-dom";
 // import CSS file for App.jsx
 import "./App.css";
-import Landing from "./screens/Landing/Landing";
-
-
 
 //Importing Screens
-import Landing from './screens/Landing/Landing.jsx'
-import Profile from './screens/Profile/Profile.jsx'
-import Home from './screens/Home/Home.jsx'
-
-import {Route, Routes} 'react-router-dom'
+import Landing from "./screens/Landing/Landing.jsx";
+import Profile from "./screens/Profile/Profile.jsx";
+import Home from "./screens/Home/Home.jsx";
 
 // Import React
-import { useEffect, useState, } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  return(
+  return (
     <section className="app">
       <Routes>
         <Route path="/sign-in" element={<Landing />} />
