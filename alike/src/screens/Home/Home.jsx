a378cf40dfd5dbf63215a06979c75efa25367b20
argurjanaolloni <argurjanaolloni@gmail.com>
2023-02-10 10:57:08 -0500

    creates Right Navbar component

--- alike/src/screens/Home/Home.jsx
+++ alike/src/screens/Home/Home.jsx
@@ -1,26 +1,27 @@
 // import React from "react";
 // import "./home.css";
 // import { Link } from "react-router-dom";
+// import RightNavbar from "../../components/RightNavbar/RightNavbar";
 
 // function Home() {
 //   return (
-//     <section className="home">
-//       <h1>Hello Im HOME</h1>
-//       <header className="home-header">
-//         <img src="./image/logo.png" className="home-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="home-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </section>
+    //     <section className="home">
+    //       <h1>Hello Im HOME</h1>
+    //       <header className="home-header">
+    //         <img src="./image/logo.png" className="home-logo" alt="logo" />
+    //         <p>
+    //           Edit <code>src/App.js</code> and save to reload.
+    //         </p>
+    //         <a
+    //           className="home-link"
+    //           href="https://reactjs.org"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           Learn React
+    //         </a>
+    //       </header>
+    //     </section>
 //   );
 // }
 
