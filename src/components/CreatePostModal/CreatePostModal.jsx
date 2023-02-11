3420e8a56b91ed5a05aead017a652bb5f971c4a9
Jagerziel <fromnytosg@gmail.com>
2023-02-11 12:50:32 -0500

    Updated skeleton code for modal

--- src/components/CreatePostModal/CreatePostModal.jsx
+++ src/components/CreatePostModal/CreatePostModal.jsx
@@ -1,2 +1,23 @@
+// Import React
+import { useEffect , useState , useContext } from 'react'
+
+// Import CSS
 import "./CreatePostModal.css";
 
+//Build CreatePostModal Component
+export default function CreatePostModal() {
+
+    return (
+        <>
+            <div className='ModalPageContainer'>
+                <div className='ModalFrame'>
+                    <div className='Modal Content'>
+                        <form action="submit">
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
