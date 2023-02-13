f33464ec33bf36b4cbc3d2e9f8b91a29ef7196ba
Jagerziel <fromnytosg@gmail.com>
2023-02-13 09:39:52 -0500

    Testing back-end integration to front-end

--- src/Hooks/usePostData.js
+++ src/Hooks/usePostData.js
@@ -4,40 +4,40 @@ export default function usePostData() {
     const [postData, setPostData] = useState(null);
 
     useEffect(() => {
-        //INSERT API LINK
-        fetch(`https://backend-alike.herokuapp.com/post/`)
-        .then(res => res.json())
-        .then(data => {
-            //Maps the data with the new object and data
-            let allPosts = data.map((post) => {
-                //Returns object with data
-                return {
-                    image: data['image'],
-                    username: data['username'],
-                    projTitle: data['projTitle'],
-                    github: data['github']
-                }
-            });
-            //Sets data
-            setPostData(allPosts)
-            })
+    //     //INSERT API LINK
+    //     fetch(`https://backend-alike-staging.herokuapp.com/post/`)
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         //Maps the data with the new object and data
+    //         let allPosts = data.map((post) => {
+    //             //Returns object with data
+    //             return {
+    //                 image: data['image'],
+    //                 username: data['username'],
+    //                 projTitle: data['projTitle'],
+    //                 github: data['github']
+    //             }
+    //         });
+    //         //Sets data
+    //         setPostData(allPosts)
+    //         })
 
 
-        // let data = [
-        //     {
-        //         image: "abc",
-        //         username: "abc",
-        //         projTitle: "title",
-        //         github: "github"
-        //     },
-        //     {
-        //         image: "def",
-        //         username: "def",
-        //         projTitle: "title2",
-        //         github: "github2"
-        //     }
-        // ]
-        // setPostData(data)
+        let data = [
+            {
+                image: "abc",
+                username: "abc",
+                projTitle: "title",
+                github: "github"
+            },
+            {
+                image: "def",
+                username: "def",
+                projTitle: "title2",
+                github: "github2"
+            }
+        ]
+        setPostData(data)
 
         }, []);
     return postData;
