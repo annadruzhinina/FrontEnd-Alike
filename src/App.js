1fdaec8d058f3c3bb073b9cc34ed620556ef8235
hairyElephant <josecalderon353@gmail.com>
2023-02-14 16:42:31 -0500

    commit for pull

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Landing />} />
-        {/* <Route path="/" element={<Profile />} /> */}
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
