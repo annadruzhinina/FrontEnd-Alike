046f8b8cf8302abd444d890c9f4e02ca32354fa5
Jagerziel <fromnytosg@gmail.com>
2023-02-14 21:42:47 -0500

    Additional data setup as backup

---
+++ src/data/userData.js
@@ -0,0 +1,14 @@
+export const postData = [
+    {
+        username: "",
+        email: ""
+    },
+    {
+        username: "",
+        email: ""
+    },
+    {
+        username: "",
+        email: ""
+    }
+]
