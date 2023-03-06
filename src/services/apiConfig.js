a2a1d7efa7ba13854053d4bd2ad807db50ab97da
Anna Druzhinina <41097193+annadruzhinina@users.noreply.github.com>
2023-03-06 11:56:20 -0500

    Update apiConfig.js

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -7,8 +7,6 @@ const apiUrls = {
   // Need to change later on real link
   production: "https://backend-alike.herokuapp.com/",
   development: "https://backend-alike.herokuapp.com/",
- production: "https://backend-alike.herokuapp.com/",
-  development: "https://backend-alike.herokuapp.com/",
 };
 // Determine if production or deployment
 if (window.location.hostname === "localhost") {
