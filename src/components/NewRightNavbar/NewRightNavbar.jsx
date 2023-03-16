eb32508ad7bf2b14b1f337d804e62af49212cd87
hairyElephant <josecalderon353@gmail.com>
2023-03-16 11:27:43 -0400

    adds api function, url, and key

--- src/components/NewRightNavbar/NewRightNavbar.jsx
+++ src/components/NewRightNavbar/NewRightNavbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import { useEffect, React } from "react";
+import axios from "axios";
+// the apiKey: 40ae771bf4ab4646b2d0c3916a06e6dd
 
 function NewRightNavbar() {
+  async function getNews() {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=40ae771bf4ab4646b2d0c3916a06e6dd"
+      );
+    } catch {}
+  }
 
-  return (
-    <div>
-        New Right Navbar
-
-    </div>
-  )
+  return <div>New Right Navbar</div>;
 }
 
-export default NewRightNavbar
+export default NewRightNavbar;
