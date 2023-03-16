b4467ee85558281080e19cf7b50bf6f1a6a33897
hairyElephant <josecalderon353@gmail.com>
2023-03-16 12:09:26 -0400

    trouble storing data to state

--- src/components/NewRightNavbar/NewRightNavbar.jsx
+++ src/components/NewRightNavbar/NewRightNavbar.jsx
@@ -1,24 +1,35 @@
-import { useEffect, React } from "react";
+import { useEffect, useState, React } from "react";
 import axios from "axios";
 // the apiKey: 40ae771bf4ab4646b2d0c3916a06e6dd
 
 function NewRightNavbar() {
+  // useState for storing data retreived from fetch call
+  const [newsData, setNewsData] = useState({});
+
+  // Function fetching news data from api
   async function getNews() {
     try {
       const response = await axios.get(
         "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=40ae771bf4ab4646b2d0c3916a06e6dd"
       );
-      console.log(response);
+      //   console.log(response);
+      setNewsData(response);
+      console.log(newsData);
     } catch (error) {
       console.error(error);
     }
   }
 
+  // useEffect for fetching news data on page load.
   useEffect(() => {
     getNews();
   }, []);
 
-  return <div>New Right Navbar</div>;
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
 }
 
 export default NewRightNavbar;
