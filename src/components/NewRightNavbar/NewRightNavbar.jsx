45be1120335307425c0e1f47de4010cc6781c53b
hairyElephant <josecalderon353@gmail.com>
2023-03-17 11:49:04 -0400

    data now visible on screen

--- src/components/NewRightNavbar/NewRightNavbar.jsx
+++ src/components/NewRightNavbar/NewRightNavbar.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState, React } from "react";
 import axios from "axios";
 // the apiKey: 40ae771bf4ab4646b2d0c3916a06e6dd
+import "./newrightnavbar.css";
 
 function NewRightNavbar() {
   // useState for storing data retreived from fetch call
-  const [newsData, setNewsData] = useState({});
+  const [newsData, setNewsData] = useState([]);
 
   // Function fetching news data from api
   async function getNews() {
@@ -12,7 +13,7 @@ function NewRightNavbar() {
       const response = await axios.get(
         "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=40ae771bf4ab4646b2d0c3916a06e6dd"
       );
-      setNewsData(response);
+      setNewsData(response.data);
     } catch (error) {
       console.error(error);
     }
@@ -21,10 +22,26 @@ function NewRightNavbar() {
   // useEffect for fetching news data on page load.
   useEffect(() => {
     getNews();
-    console.log(newsData);
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="NRNB">
+      <h1>Tech News</h1>
+      <ul>
+        {newsData.articles ? (
+          newsData.articles.map((item, index) => (
+            <li key={index}>
+              <img src={item["urlToImage"]} alt="articlePic" />
+              <p>{item.title}</p>
+              <a href={item.url}>Read More</a>
+            </li>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </ul>
+    </div>
+  );
 }
 
 export default NewRightNavbar;
