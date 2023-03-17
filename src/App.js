25b4e44ed3c87fe7743b099e99f96591064e86d6
hairyElephant <josecalderon353@gmail.com>
2023-03-17 13:34:47 -0400

    adding setuser app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import React
 import { Route, Routes } from "react-router-dom";
-import React, { createContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getUser } from './services/userApi.js';
 // import CSS file for App.jsx
 // import "./App.css";
@@ -13,32 +13,30 @@ import SignUp from "./components/SignUp/SignUp.jsx";
 // importing newrightnavbar temporarily so it can be worked on
 import NewRightNavbar from "./components/NewRightNavbar/NewRightNavbar.jsx";
 
-export const UserContext = React.createContext()
-const [user, setUser] = useState(null)
-
-useEffect(()=>{
-    const fetchUser = async () => {
-        let response = await getUser()
-        setUser(response)
-    }
-
-    fetchUser()
-}, [])
 
 
 function App() {
-  let data = ""
-  return (
-    <>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/newrightnavbar" element = {<NewRightNavbar/>} />
-      </Routes>
-    </>
-  );
+    const [user, setUser] = useState(null)
+    
+    useEffect(()=>{
+        const fetchUser = async () => {
+            let response = await getUser()
+            setUser(response)
+        }
+    
+        fetchUser()
+    }, [])
+    return (
+        <>
+            <Routes>
+                <Route path="/home" element={<Home user={user}/>} />
+                <Route path="/" element={<Landing  setUser={setUser}/>} />
+                <Route path="/sign-up" element={<SignUp setUser={setUser}/>} />
+                <Route path="/profile" element={<Profile user={user}/>} />
+                <Route path="/newrightnavbar" element = {<NewRightNavbar/>} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
