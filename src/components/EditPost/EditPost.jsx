90c60b7fddf8580440a67bded4665d5cba5d2f47
Jagerziel <fromnytosg@gmail.com>
2023-03-24 14:47:44 -0400

    Splitting out Edit Post to new component

---
+++ src/components/EditPost/EditPost.jsx
@@ -0,0 +1,15 @@
+//Import React
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+//Import ccs
+import "./editPost.css";
+
+function EditPost() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default EditPost
