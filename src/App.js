b09f2aa08d54c055f3cf1fbf873de85d7f64b1d1
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-04-14 10:09:31 -0400

    fix post width on search

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@
 import { Route, Routes } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 import { getUser } from './services/userApi.js'
-// import CSS file for App.jsx
-// import "./App.css";
 
 //Importing Screens
 import Landing from './screens/Landing/Landing.jsx'
 import Profile from './screens/Profile/Profile.jsx'
 import Home from './screens/Home/Home.jsx'
 import SignUp from './components/SignUp/SignUp.jsx'
-// importing RightNavbar temporarily so it can be worked on
 import RightNavbar from './components/RightNavbar/RightNavbar.jsx'
 
 function App() {
